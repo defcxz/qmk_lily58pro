40d41a95813102f21012f5feb8f1fed2c344abe6
Mario Gómez <ma.gomez@duocuc.cl>
2022-12-31 22:18:06 +0000

    My layout

--- keyboards/lily58/keymaps/default/rules.mk
+++ keyboards/lily58/keymaps/default/rules.mk
@@ -1,20 +1,21 @@
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+# BACKLIGHT_ENABLE = yes    # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE= yes     # OLED display
+OLED_ENABLE= yes            # OLED display
+WPM_ENABLE = yes            # Enable WPM counter
+TAP_DANCE_ENABLE = yes      # Enable Tap dance
+EXTRAFLAGS += -flto         # Enable link time optimization (saves a lot of space)
+COMMAND_ENABLE = no         # Enable command
+UNICODE_ENABLE = no         # Enable unicode
+CONSOLE_ENABLE = no         # Enable console
+SPACE_CADET_ENABLE = no     # Enable space cadet
+MAGIC_ENABLE = no           # Enable magic
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
         ./lib/keylogger.c \
+        # ./lib/logo_reader.c \
+        # ./lib/timelogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
